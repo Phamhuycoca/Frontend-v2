5b71002fdac13d3dee1539cff6681a226c34c740
Phamhuycoca <phamkhachuy240702@gmail.com>
2025-04-08 23:30:35 +0700

    first commit

---
+++ src/components/Button.tsx
@@ -0,0 +1,9 @@
+export const Button = () => {
+    return (
+        <>
+            <button type="button">Submit</button>
+            <button type="reset">Reset</button>
+            <h1>Ok</h1>
+        </>
+    );
+};
