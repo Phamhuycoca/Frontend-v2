73656ab0e448c36e6ee0f50c3191d872c000ddaa
Phamhuycoca <Phamkhachuy240702@gmail.com>
2025-04-28 13:17:04 +0700

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from 'path'
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'public/web.config',
+          dest: ''
+        }
+      ]
+    })
+  ],
   server: {
     port: 2407
   },
